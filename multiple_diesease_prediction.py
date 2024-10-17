6e03431f94f83e0b6d9142948c1db5210fa57bec
Nimra Idris Siddiqui <80575499+nimrausa@users.noreply.github.com>
2024-10-17 02:16:02 +0000

    commit

--- multiple_diesease_prediction.py
+++ multiple_diesease_prediction.py
@@ -57,11 +57,16 @@ def display_reviews_and_ratings(selected_disease):
 def load_model(file_path):
     return pickle.load(open(file_path, 'rb'))
 
-#loading the saved models
-diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
-heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
-parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
+# getting the working directory of the main.py
+working_dir = os.path.dirname(os.path.abspath(__file__))
 
+# loading the saved models
+
+diabetes_model = pickle.load(open(f'{working_dir}/saved_models/diabetes_model.sav', 'rb'))
+
+heart_disease_model = pickle.load(open(f'{working_dir}/saved_models/heart_disease_model.sav', 'rb'))
+
+parkinsons_model = pickle.load(open(f'{working_dir}/saved_models/parkinsons_model.sav', 'rb'))
 
 
 
